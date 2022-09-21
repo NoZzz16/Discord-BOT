1ebad95136656d78b1b0a90446efad8bd2fd415f
NoZzz16 <nozzz@LAPTOP-GT91L14R.localdomain>
2022-09-21 21:51:14 +0200

    5 features done + clean code

--- src/main.py
+++ src/main.py
@@ -20,36 +20,36 @@ async def on_ready():  # When the bot is ready
     print("I'm in")
     print(bot.user)  # Prints the bot's username and identifier
 
-@bot.command()
+@bot.command() #This is the !name feature
 async def name(ctx):
     username = ctx.author.name
     await ctx.send(username)
 
-@bot.command()
+@bot.command() #This is the !d6 feature
 async def d6(ctx):
     rand = randint(1,6)
     await ctx.send(rand)
 
-@bot.event
+@bot.event #This is the "Salut tout le monde" feature
 async def on_message(message):
     if message.content == "Salut tout le monde" :
         user = message.author.mention
         await message.channel.send(f"Salut tout seul {user}")
     await bot.process_commands(message)
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True) #Give a role to a member
 async def giverole(ctx, user: discord.Member, role: discord.Role):
     await user.add_roles(role)
     await ctx.send(f"hey {ctx.author.name}, {user.name} made u a : {role.name}")
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True) #Create the "Admin" role, with all permissions allowed
 async def create_role(ctx):
 	guild = ctx.guild
 	await guild.create_role(name="Admin", permissions=Permissions.all())
 	await ctx.send(f'Role Admin has been created')
 
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True) #This is the !admin feature
 async def admin(ctx, member: discord.Member):
     if get(ctx.guild.roles, name="Admin") :
         await giverole(ctx, member, get(ctx.guild.roles, name="Admin"))
@@ -57,7 +57,7 @@ async def admin(ctx, member: discord.Member):
         await create_role(ctx)
         await giverole(ctx, member, get(ctx.guild.roles, name="Admin"))
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True) #This is the ban feature
 async def ban(ctx, member: discord.Member) :
     await ctx.guild.ban(member, reason="Cheh")
     await ctx.send(f"{member} has been successfully banned.")
@@ -66,5 +66,5 @@ async def ban(ctx, member: discord.Member) :
 async def pong(ctx):
     await ctx.send('pong')
 
-token = ""
+token = "MTAyMjE5MzE0NzU3ODY5MTYxNA.GU_4lE.Kci_TVjr4bgUp1cSvA6VIdC7ImM-J40C898-RE"
 bot.run(token)  # Starts the bot
